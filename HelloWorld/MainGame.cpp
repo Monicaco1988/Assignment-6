499136c501194e7ddc32c9c7ec4db09f287d6236
Monicaco1988 <edmin.aguilaroddershede@gmail.com>
2023-12-12 14:01:22 +0100

    commit DoublyLinkedList

--- HelloWorld/MainGame.cpp
+++ HelloWorld/MainGame.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 
+using namespace std;
+
+// single nexted list
 
 
-
-void main()
+class Node
 {
+public:
+	int data;
+	Node* next; // 
+	Node* prev;
+};
 
+//typedef Node* nodePtr;
+
+void insert(Node*& head, int data)
+{
+	Node* tempPtr;
+	tempPtr = new Node;
+	tempPtr->data = data;
+	tempPtr->next = head;
+	head = tempPtr;
+}
+
+
+
+int main()
+{
+	Node* head;
+	head = new Node;
+	head->data = 20; // using arrow to populate data in the "headNode" 
+	head->next = NULL;
+	//cout << head->data << endl;
+
+	insert(head, 30);
+
+	insert(head, 40);
+
+	Node* tmp;
+	tmp = head;
+
+	while (tmp != NULL)
+	{
+		cout << tmp->data << endl;
+		tmp = tmp->next;
+
+	}
 
 }
